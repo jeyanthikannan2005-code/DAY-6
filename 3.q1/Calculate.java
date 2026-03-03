55497c5c0933e86fc59e6cd68ab36df07feb3db0
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-03-03 13:07:34 +0530

    Arithmetic Operator Program

---
+++ 3.q1/Calculate.java
@@ -0,0 +1,20 @@
+class Calculate
+{
+	public static void main(String[] args)
+	{
+		int a=20;
+		int b=10;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=a/b;
+		int g=a%b;
+		System.out.println("The value of a :"+a);
+		System.out.println("The value of b :"+b);
+		System.out.println("The Sum is :"+c);
+		System.out.println("The difference is :"+d);
+		System.out.println("The product is :"+e);
+		System.out.println("The division is :"+f);
+		System.out.println("The modolus is :"+g);
+	}
+}
