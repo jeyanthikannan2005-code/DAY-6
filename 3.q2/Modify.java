1e8df1a20b19095202548ce32daf7da0618dbedd
Jeyanthi Kannan <jeyanthikannan2005@gmail.com>
2026-03-03 13:09:25 +0530

    Assignment Operator Program

---
+++ 3.q2/Modify.java
@@ -0,0 +1,13 @@
+class Modify
+{
+	public static void main(String[] args)
+	{
+		int a=10;
+		a+=10;
+		a-=15;
+		a*=5;
+		a/=5;
+		a%=5;
+		System.out.println("The initial value of a is :10"+"\nThe value of a after modification is :"+a); 
+	}
+}
